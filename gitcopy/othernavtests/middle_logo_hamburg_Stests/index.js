a262e7f2cb4f8ee62aeed26dcbda975a4feef149
dsAIRBOURNE <82331958+desariah-airbourne@users.noreply.github.com>
2023-11-16 22:03:24 +0000

    webupgrade

---
+++ gitcopy/othernavtests/middle_logo_hamburg_Stests/index.js
@@ -0,0 +1,22 @@
+const $scroll = $('#bottom-left');
+
+$(window).on("scroll", function(){
+  const st = $(this).scrollTop();
+  if( st >= 240){
+    $scroll.css({position: "absolute", top: 330});  
+  }else{
+    $scroll.css({position: "fixed", top: 100});  
+  }       
+});
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function accountCheck1() {}
