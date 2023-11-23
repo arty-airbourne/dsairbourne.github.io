08d3b9fe7541f21f2fa413d1d2caa4466e49f5ed
dsAIRBOURNE <82331958+desariah-airbourne@users.noreply.github.com>
2023-11-23 20:01:06 +0000

    added new carousel?

--- gitcopy/OtherTests/p/mag/carouseltest.js
+++ gitcopy/OtherTests/p/mag/carouseltest.js
@@ -82,7 +82,6 @@ var bMobile =   // will be true if running on a mobile device
   Draggable.create(proxy, {
     type: "x",
     trigger: ".wrapper",
-    inertia: true,
     onDrag: updateProgress,
     onThrowUpdate: updateProgress
   });
@@ -116,3 +115,27 @@ var bMobile =   // will be true if running on a mobile device
   
   // AUTOPLAY
   animation.play();
+
+  function Marquee(selector, speed) {
+    const parentSelector = document.querySelector(selector);
+    const clone = parentSelector.innerHTML;
+    const firstElement = parentSelector.children[0];
+    let i = 0;
+    console.log(firstElement);
+    parentSelector.insertAdjacentHTML('beforeend', clone);
+    parentSelector.insertAdjacentHTML('beforeend', clone);
+  
+    setInterval(function () {
+      firstElement.style.marginLeft = `-${i}px`;
+      if (i > firstElement.clientWidth) {
+        i = 0;
+      }
+      i = i + speed;
+    }, 0);
+  }
+  
+  //after window is completed load
+  //1 class selector for marquee
+  //2 marquee speed 0.2
+  window.addEventListener('load', Marquee('.marquee', 0.2))
+  
