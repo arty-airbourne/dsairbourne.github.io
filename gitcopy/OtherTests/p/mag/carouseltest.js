f37ec43714f71dc5ad80a3bff1216213bd7cbac8
dsAIRBOURNE <82331958+desariah-airbourne@users.noreply.github.com>
2023-11-23 23:15:28 +0000

    fix+cleanup

--- gitcopy/OtherTests/p/mag/carouseltest.js
+++ gitcopy/OtherTests/p/mag/carouseltest.js
@@ -1,21 +1,3 @@
-const $scroll = $('#bottom-left');
-
-$(window).on("scroll", function(){
-  const st = $(this).scrollTop();
-  if( st >= 240){
-    $scroll.css({position: "absolute", top: 330});  
-  }else{
-    $scroll.css({position: "fixed", top: 100});  
-  }       
-});
-
-var bMobile =   // will be true if running on a mobile device
-  navigator.userAgent.indexOf( "Mobile" ) !== -1 || 
-  navigator.userAgent.indexOf( "iPhone" ) !== -1 || 
-  navigator.userAgent.indexOf( "Android" ) !== -1 || 
-  navigator.userAgent.indexOf( "Windows Phone" ) !== -1 ;
-
-
   function Marquee(selector, speed) {
     const parentSelector = document.querySelector(selector);
     const clone = parentSelector.innerHTML;
