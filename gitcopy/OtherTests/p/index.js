ca50d2706e63db694f18f6ee7367e0a8a9bdf03a
dsAIRBOURNE <82331958+desariah-airbourne@users.noreply.github.com>
2023-11-25 23:55:19 +0000

    things yk

--- gitcopy/OtherTests/p/index.js
+++ gitcopy/OtherTests/p/index.js
@@ -15,3 +15,15 @@ var bMobile =   // will be true if running on a mobile device
   navigator.userAgent.indexOf( "Android" ) !== -1 || 
   navigator.userAgent.indexOf( "Windows Phone" ) !== -1 ;
 
+  var inputs = document.getElementsByTagName('input');
+
+  for (var i = 0, len = inputs.length; i < len; i++) {
+      input = inputs[i];
+      input.onmouseover = function(){
+          this.setAttribute('data-orig-image',this.getAttribute('src'));
+          this.src = this.getAttribute('data-alt-image');
+      };
+      input.onmouseout = function(){
+          this.src = this.getAttribute('data-orig-image');
+      };
+  }
