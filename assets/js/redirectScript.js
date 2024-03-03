203616a0e37d147005ad0952732172d87e41e197
Anthony Folley <anthony@folley.io>
2024-03-03 14:38:41 +0000

    Update redirectScript.js

--- assets/js/redirectScript.js
+++ assets/js/redirectScript.js
@@ -1,3 +1,15 @@
+window.onload = checkwidth();
+
+function checkwidth() { 
+    if($(window).width() > 950) {
+        window.location = "https://www.airbourne.xyz/d/index.html";
+    } else if ($(window).width() < 950) {
+        window.location = "https://www.airbourne.xyz/m/index.html";
+    };
+
+};
+
+
 $(window).on('load resize',function(){
     if($(window).width() < 950){
         window.location = "https://www.airbourne.xyz/m/index.html"
